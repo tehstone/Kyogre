e628cf05f216789c1b78d2197a60baa9bfa84948
tehstone <stone.johnny@gmail.com>
2019-07-20 14:41:02 -0700

    Give badge to role command added. Help text added to badge commands

--- kyogre/exts/badges.py
+++ kyogre/exts/badges.py
@@ -64,9 +64,11 @@ class Badges(commands.Cog):
         await ctx.message.add_reaction(reaction)
         await ctx.channel.send(embed=discord.Embed(colour=colour, description=message), delete_after=12)
 
-    @_badge.command(name='grant', aliases=['give', 'gr'])
+    @commands.command(name='grant_badge', aliases=['give', 'gb'])
     @commands.has_permissions(manage_roles=True)
-    async def _grant(self, ctx, badge_id: int = 0, *, member):
+    async def _grant_badge(self, ctx, badge_id: int = 0, *, member):
+        """**Usage**: `!grant_badge/give/gb <badge_id> <member>`
+        Gives the provided badge to the provided user."""
         converter = commands.MemberConverter()
         try:
             member = await converter.convert(ctx, member)
@@ -97,8 +99,54 @@ class Badges(commands.Cog):
         await ctx.message.add_reaction(reaction)
         await ctx.channel.send(embed=discord.Embed(colour=colour, description=message), delete_after=12)
 
-    @commands.command(name="available-badges", aliases=['ab'])
+    @commands.command(name='grant_to_role', aliases=['givetr', 'gbtr'])
+    @commands.has_permissions(manage_roles=True)
+    async def _grant_to_role(self, ctx, badge_id: int = 0, *, role):
+        """**Usage**: `!grant_to_role/givetr/gbtr <badge_id> <rolename>`
+        Gives the provided badge to all users assigned to the provided role."""
+        converter = commands.RoleConverter()
+        try:
+            role = await converter.convert(ctx, role)
+        except:
+            role = None
+        if badge_id == 0 or role is None:
+            await ctx.message.add_reaction(self.bot.failed_react)
+            return await ctx.send("Must provide a badge id and Role name.", delete_after=10)
+        badge_to_give = BadgeTable.get(BadgeTable.id == badge_id)
+        if badge_to_give:
+            try:
+                trainer_ids = []
+                errored = []
+                guild_obj, __ = GuildTable.get_or_create(snowflake=ctx.guild.id)
+                for trainer in role.members:
+                    try:
+                        trainer_obj, __ = TrainerTable.get_or_create(snowflake=trainer.id, guild=ctx.guild.id)
+                        trainer_ids.append((badge_to_give.id, trainer_obj.snowflake))
+                    except:
+                        errored.append(trainer.id)
+                with KyogreDB._db.atomic():
+                    BadgeAssignmentTable.insert_many(trainer_ids,
+                                                     fields=[BadgeAssignmentTable.badge_id,
+                                                             BadgeAssignmentTable.trainer]).execute()
+                message = f"Could not assign the badge to: {', '.join(errored)}"
+            except:
+                await ctx.message.add_reaction(self.bot.failed_react)
+                return await ctx.channel.send(embed=discord.Embed(colour=discord.Colour.red(),
+                                                           description="Completely failed"), delete_after=12)
+            if len(errored) > 0:
+                colour = discord.Colour.from_rgb(255, 255, 0)
+                await ctx.message.add_reaction(self.bot.failed_react)
+                await ctx.message.add_reaction(self.bot.success_react)
+                return await ctx.channel.send(embed=discord.Embed(colour=colour, description=message), delete_after=12)
+            colour = discord.Colour.green()
+            message = "Successfully granted badge."
+            await ctx.message.add_reaction(self.bot.success_react)
+            return await ctx.channel.send(embed=discord.Embed(colour=colour, description=message), delete_after=12)
+
+    @commands.command(name="available_badges", aliases=['avb'])
     async def _available(self, ctx):
+        """**Usage**: `!available_badges/avb`
+        Lists all badges that are currently available."""
         result = (BadgeTable
                   .select(BadgeTable.id,
                           BadgeTable.name,
@@ -116,6 +164,8 @@ class Badges(commands.Cog):
 
     @commands.command(name="badges")
     async def _badges(self, ctx):
+        """**Usage**: `!badges`
+        Shows all badges earned by whomever sent the command."""
         author = ctx.message.author
         badges = self.get_badges(author.id)
         embed = discord.Embed(title=f"{author.display_name} has earned {len(badges)} badges", colour=author.colour)
